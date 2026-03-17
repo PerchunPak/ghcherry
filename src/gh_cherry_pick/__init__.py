eaf2855e7d63e6396cb2673d54b49b47669a3ca0
Perchun Pak <git@perchun.it>
2026-03-17 22:45:28 +0100

    Add basic CLI

--- src/gh_cherry_pick/__init__.py
+++
@@ -1,2 +0,0 @@
-def hello() -> str:
-    return "Hello from gh-cherry-pick!"
---
+++ src/gh_cherry_pick/__main__.py
@@ -0,0 +1,36 @@
+import os
+import typing as t
+
+import cyclopts
+
+app = cyclopts.App(name="gh-cherry-pick", version_flags=[])
+
+
+@app.default
+def main(
+    *commits: t.Annotated[
+        str,
+        cyclopts.Parameter(help="Commits to cherry-pick", required=True),
+    ],
+    github_token: t.Annotated[
+        str | None,
+        cyclopts.Parameter(
+            name=["-t", "--token"],
+            help=(
+                "GitHub token, if not specified, fallbacks to the "
+                + "$GITHUB_TOKEN environment variable"
+            ),
+        ),
+    ] = None,
+) -> None:
+    if not github_token:
+        github_token = os.environ.get("GITHUB_TOKEN")
+    if not github_token:
+        raise ValueError(
+            "You need to specify GitHub token either using --token "
+            + "parameter or $GITHUB_TOKEN environment variable"
+        )
+
+
+if __name__ == "__main__":
+    app()
