2f28b0f0f625c2ed96ba39ac4a6beccb4a9f54f9
Perchun Pak <git@perchun.it>
2026-03-17 10:37:24 +0100

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# gh-cherry-pick is now ghcherry
+
+This package has been renamed. Use `pip install ghcherry` instead.
+
+New package: https://pypi.org/project/ghcherry/
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup
+
+VERSION = "1.6.0"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="gh-cherry-pick",
+    description="gh-cherry-pick is now ghcherry",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    install_requires=["ghcherry==" + VERSION],
+    classifiers=["Development Status :: 7 - Inactive"],
+)
